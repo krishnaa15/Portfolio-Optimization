cc5e31c230685087cdd7dd64b735c84a0b7e5eb2
divyangana009 <87435764+divyangana009@users.noreply.github.com>
2023-09-24 16:18:39 +0530

    Add files via upload

---
+++ portfolio-optimization/samples/grab_price_data.py
@@ -0,0 +1,21 @@
+import sys
+from pprint import pprint
+from fake_useragent import UserAgent
+sys.path.insert(0, 'C:/Users/91880/Desktop/portfolio-optimization/pyopt')
+from client import PriceHistory
+
+ # Initialize the client.
+price_history_client = PriceHistory(
+    symbols=['AAPL','MSFT','SQ'],
+    user_agent=UserAgent().edge
+)
+
+# Dump it to a CSV file.
+price_history_client.price_data_frame.to_csv(
+    'data/stock_data.csv',
+    index=False
+)
+pprint(price_history_client.price_data_frame)
+
+# Grab the data frame.
+price_data_frame = price_history_client.price_data_frame
